bd18e1babce070f6d074962351d09255166655be
mahesh <rudrakshalamahesh12@gmail.com>
2024-01-12 09:18:44 +0530

    test

---
+++ my-web-app/src/main/java/com/example/MyServlet.java
@@ -0,0 +1,23 @@
+// src/main/java/com/example/MyServlet.java
+package com.example;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServlet", value = "/hello")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, this is my servlet!</h1>");
+        out.println("</body></html>");
+    }
+}
+
